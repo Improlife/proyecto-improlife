5b05d6d9e8eaed8111e8be2b5c701e366dadfae8
Daniel <improlife25@gmail.com>
2025-11-30 13:00:33 +0100

    Actualizados archivos HTML y enlaces para GitHub Pages

--- documentos-js/enviar-formulario.js
+++ documentos-js/enviar-formulario.js
@@ -1,17 +1,18 @@
- // Inicialización
-    emailjs.init("tDm1ziFTtxw3FuRwk");
+emailjs.init("tDm1ziFTtxw3FuRwk");
 
-    // Envío del formulario
-    document.getElementById("formContacto").addEventListener("submit", function(event) {
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("formularioContacto");
+    form.addEventListener("submit", function(event) {
         event.preventDefault();
-
         emailjs.sendForm("service_6k9v1ph", "template_8opb29m", this)
-            .then(() => {
+            .then((response) => {
+                console.log("EmailJS response:", response);
                 alert("Mensaje enviado correctamente.");
-                this.reset();
+                form.reset();
             })
             .catch((error) => {
-                alert("Error al enviar el formulario.");
-                console.error(error);
+                console.error("EmailJS error:", error);
+                alert("Error al enviar el formulario. Mira la consola.");
             });
     });
+});
